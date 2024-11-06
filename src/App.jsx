1cb6e374b09a53896f1d4a09683a425dde6aaed0
Rodrigo <rzunigamadrid@gmail.com>
2024-11-06 20:53:27 -0300

    Template Parcial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Card from "./components/Card"
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>Carga de Estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  ) 
+}
+
+export default App
